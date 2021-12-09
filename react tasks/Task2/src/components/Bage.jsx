61e8e5d32b8d942207c02d8fff7937ed86dbb07f
yousef ahmed <yousef.amer5453@gmail.com>
2021-12-09 15:56:58 +0200

    first commit

---
+++ react tasks/Task2/src/components/Bage.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import './style.css'
+
+export default class Bage extends Component {
+  render() {
+
+    return
+    <React.Fragment>
+    
+    <div class="header">
+        <h1>tawfik shalash</h1>
+        <p>this is my offcial wep site you are welcome</p>
+    </div>
+
+    <div class="topnav">
+        <a href="https://www.facebook.com/tawfeek.shalash/" style="color: white;">facebook</a>
+        <a href="https://twitter.com/ShalashTawfik" style="color: white;">twitter</a>
+        <a href="https://www.instagram.com/tawfeekshalash/" style="color: white;">instagram</a>
+        <a href="https://www.linkedin.com/in/tawfik-shalash-6517ab1b3/" style="color: white;">linkedIn</a>
+    </div>
+
+
+    <div class="row">
+        <div class="leftcolumn">
+            <div class="card">
+                <h2>ECPC day</h2>
+                <h5>me in ECPC day 2021</h5>
+                <img src="pic/ECPC.jpg" alt="ecpc picture"></img>
+                <p>teams which qualified to ecpc</p>
+                <p>this picture taked after ECPC day </p>
+            </div>
+            <div class="card">
+                <h2>ECPC qualification</h2>
+                <h5>me in ECPC 2021</h5>
+                <img src="pic/ECPCqulification.jpg" alt="ecpc picture"></img>
+                <p>a fantastic day with my team</p>
+                <p>in this moment i was so happy to be qualified to ECPC </p>
+            </div>
+            <div class="card">
+                <h2>ECPC qualification 2022</h2>
+                <h5>me in ECPC 2020</h5>
+                <img src="pic/ECPC2020.jpg" alt="ecpc picture"></img>
+                <p>a fantastic day with my team</p>
+                <p>in this moment i was so happy to be qualified to ECPC </p>
+            </div>
+            <div class="card">
+                <h2>my internship at fatura</h2>
+                <img src="pic/internship.jpg" alt="internship"></img>
+                <p>frist day at the intern ship at fatura</p>
+                <p>3 months in the fatura company madi </p>
+            </div>
+
+        </div>
+
+        <div class="rightcolumn">
+            <div class="card">
+                <h2>Me</h2>
+
+            </div>
+            <div class="card">
+                <h3>Popular Post</h3>
+                <div class="fakeimg">
+                    <p>Image</p>
+                </div>
+                <div class="fakeimg">
+                    <p>Image</p>
+                </div>
+                <div class="fakeimg">
+                    <p>Image</p>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+    <div class="footer">
+        <a href="https://www.facebook.com/tawfeek.shalash/">facebook</a>
+        <a href="https://twitter.com/ShalashTawfik">twitter</a>
+        <a href="https://www.instagram.com/tawfeekshalash/">instagram</a>
+        <a href="https://www.linkedin.com/in/tawfik-shalash-6517ab1b3/">linkedIn</a>
+    </div>
+
+</React.Fragment>;
+
+  }
+
+}
+
+
+
+
